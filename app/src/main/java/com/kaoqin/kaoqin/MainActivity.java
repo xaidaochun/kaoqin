9902c4cf7d3e2a58c2a11f1dd960cef29b49a3cc
xia <843098885@qq.com>
2018-05-26 05:45:39 +0800

    第2次提交

--- app/src/main/java/com/kaoqin/kaoqin/MainActivity.java
+++ app/src/main/java/com/kaoqin/kaoqin/MainActivity.java
@@ -2,12 +2,13 @@ package com.kaoqin.kaoqin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+public TextView tv;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=(TextView) findViewById(R.id.textView);
     }
 }
